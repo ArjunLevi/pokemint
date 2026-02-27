6f6be3a0978e10867a98af6db8e1d75ec0d7bf98
ArjunLevi <66naga18@gmail.com>
2026-02-27 12:54:34 +0530

    Update MetaData

--- packages/nextjs/utils/tokenization/nftsMetadata.ts
+++ packages/nextjs/utils/tokenization/nftsMetadata.ts
@@ -1,122 +1,294 @@
 const nftsMetadata = [
   {
-    description: "It's actually a bison?",
-    external_url: "https://austingriffith.com/portfolio/paintings/", // <-- this can link to a page for the specific file too
-    image: "https://austingriffith.com/images/paintings/buffalo.jpg",
-    name: "Buffalo",
+    name: "Iron-Bark Sand-Shredder",
+    description: "A metallic desert guardian whose scales are reinforced with carbon-fiber plates.",
+    image:
+      "https://orange-hidden-sloth-723.mypinata.cloud/ipfs/bafybeiebe7ydpdcyuvll36zt4e6l2232jnytkbectiiyjli3pgonb72d5e",
     attributes: [
-      {
-        trait_type: "BackgroundColor",
-        value: "green",
-      },
-      {
-        trait_type: "Eyes",
-        value: "googly",
-      },
-      {
-        trait_type: "Stamina",
-        value: 42,
-      },
+      { trait_type: "Type", value: "Earth/Metal" },
+      { trait_type: "Power", value: 85 },
     ],
   },
   {
-    description: "What is it so worried about?",
-    external_url: "https://austingriffith.com/portfolio/paintings/", // <-- this can link to a page for the specific file too
-    image: "https://austingriffith.com/images/paintings/zebra.jpg",
-    name: "Zebra",
+    name: "Crimson Aero-Scythe",
+    description: "An high-speed aerial predator equipped with jet-boosted wings and titanium claws.",
+    image:
+      "https://orange-hidden-sloth-723.mypinata.cloud/ipfs/bafybeidr5bajlffjpl3x7uso4eyhrmmv2gyc3jrsm6juiqfa4ui4636i7a",
     attributes: [
-      {
-        trait_type: "BackgroundColor",
-        value: "blue",
-      },
-      {
-        trait_type: "Eyes",
-        value: "googly",
-      },
-      {
-        trait_type: "Stamina",
-        value: 38,
-      },
+      { trait_type: "Type", value: "Fire/Steel" },
+      { trait_type: "Power", value: 92 },
     ],
   },
   {
-    description: "What a horn!",
-    external_url: "https://austingriffith.com/portfolio/paintings/", // <-- this can link to a page for the specific file too
-    image: "https://austingriffith.com/images/paintings/rhino.jpg",
-    name: "Rhino",
+    name: "Spark-Core Raichu-Bot",
+    description: "A heavy-duty electric sentinel that stores raw lightning in its external battery packs.",
+    image:
+      "https://orange-hidden-sloth-723.mypinata.cloud/ipfs/bafybeihzhkm5qup2eo7pq76n6lt42ur6gokgeand5xbpnhcygfbc4u5wue",
     attributes: [
-      {
-        trait_type: "BackgroundColor",
-        value: "pink",
-      },
-      {
-        trait_type: "Eyes",
-        value: "googly",
-      },
-      {
-        trait_type: "Stamina",
-        value: 22,
-      },
+      { trait_type: "Type", value: "Electric" },
+      { trait_type: "Power", value: 88 },
     ],
   },
   {
-    description: "Is that an underbyte?",
-    external_url: "https://austingriffith.com/portfolio/paintings/", // <-- this can link to a page for the specific file too
-    image: "https://austingriffith.com/images/paintings/fish.jpg",
-    name: "Fish",
+    name: "Android Gardevoir-X",
+    description: "A refined psychic entity whose consciousness has been digitized for eternal combat.",
+    image:
+      "https://orange-hidden-sloth-723.mypinata.cloud/ipfs/bafybeibvfvocux4y4yc3gsfzs3fbnuu22mgfonz4ztyrxlxiwt7nztdzua",
     attributes: [
-      {
-        trait_type: "BackgroundColor",
-        value: "blue",
-      },
-      {
-        trait_type: "Eyes",
-        value: "googly",
-      },
-      {
-        trait_type: "Stamina",
-        value: 15,
-      },
+      { trait_type: "Type", value: "Psychic/Cyber" },
+      { trait_type: "Power", value: 95 },
     ],
   },
   {
-    description: "So delicate.",
-    external_url: "https://austingriffith.com/portfolio/paintings/", // <-- this can link to a page for the specific file too
-    image: "https://austingriffith.com/images/paintings/flamingo.jpg",
-    name: "Flamingo",
+    name: "Plasma-Tail Charmander",
+    description: "A tactical fire scout using a blue-flame plasma reactor to power its thermal attacks.",
+    image:
+      "https://orange-hidden-sloth-723.mypinata.cloud/ipfs/bafybeicmano5nvadhyxegqk5pblshle4dva4syati2k7xzehodealbqgau",
     attributes: [
-      {
-        trait_type: "BackgroundColor",
-        value: "black",
-      },
-      {
-        trait_type: "Eyes",
-        value: "googly",
-      },
-      {
-        trait_type: "Stamina",
-        value: 6,
-      },
+      { trait_type: "Type", value: "Fire/Tech" },
+      { trait_type: "Power", value: 70 },
     ],
   },
   {
-    description: "Raaaar!",
-    external_url: "https://austingriffith.com/portfolio/paintings/", // <-- this can link to a page for the specific file too
-    image: "https://austingriffith.com/images/paintings/godzilla.jpg",
-    name: "Godzilla",
+    name: "Abyssal-Eye Jelly-Drone",
+    description: "A deep-sea surveillance bot that emits electromagnetic pulses to stun ocean prey.",
+    image:
+      "https://orange-hidden-sloth-723.mypinata.cloud/ipfs/bafybeiez27gqofz46zx5ujnnwsqpr2cnwlpm4t2gslqlguvbybe6arepze",
     attributes: [
-      {
-        trait_type: "BackgroundColor",
-        value: "orange",
-      },
-      {
-        trait_type: "Eyes",
-        value: "googly",
-      },
-      {
-        trait_type: "Stamina",
-        value: 99,
-      },
+      { trait_type: "Type", value: "Water/Electric" },
+      { trait_type: "Power", value: 75 },
+    ],
+  },
+  {
+    name: "Solar-Wing Blaziken",
+    description: "A legendary warrior that gathers solar energy in its wings to unleash a supernova kick.",
+    image:
+      "https://azure-voluntary-cat-524.mypinata.cloud/ipfs/bafybeigpadpbcr5iab6kzpraewt2mkahckkmylhxlo7wagxw4zmunvj3ui",
+    attributes: [
+      { trait_type: "Type", value: "Fire/Legendary" },
+      { trait_type: "Power", value: 120 },
+    ],
+  },
+  {
+    name: "Cinder-Hoof Arcanine",
+    description: "A massive cyber-beast whose mane is made of pure, heat-shielded plasma.",
+    image:
+      "https://azure-voluntary-cat-524.mypinata.cloud/ipfs/bafybeietpsj2ason5relziyt4pt7iy3zdpwymbdjqse7w52ow2zglmypxq",
+    attributes: [
+      { trait_type: "Type", value: "Fire/Steel" },
+      { trait_type: "Power", value: 99 },
+    ],
+  },
+  {
+    name: "Volt-Claw Behemoth",
+    description: "A massive ground-type Pokémon reinforced with hydraulic claws and electric plating.",
+    image:
+      "https://azure-voluntary-cat-524.mypinata.cloud/ipfs/bafybeibaud7fyuxck7x57hjp2rqd42fi2ng5zyz576k53h2iamv7l5qvii",
+    attributes: [
+      { trait_type: "Type", value: "Electric/Ground" },
+      { trait_type: "Power", value: 110 },
+    ],
+  },
+  {
+    name: "Neon-Byte Umbreon",
+    description: "A sleek, dark predator whose rings pulse with bioluminescent neon data streams.",
+    image:
+      "https://azure-voluntary-cat-524.mypinata.cloud/ipfs/bafybeibijreztxbqnjsmtcozikru6rsnis55ttkgb7xr6zrkyzzfin7hbi",
+    attributes: [
+      { trait_type: "Type", value: "Dark/Cyber" },
+      { trait_type: "Power", value: 82 },
+    ],
+  },
+  {
+    name: "Aero-Scythe Mantis",
+    description: "A precision-engineered insectoid with dual vibratory blades that cut through titanium.",
+    image:
+      "https://azure-voluntary-cat-524.mypinata.cloud/ipfs/bafybeifuhy3d42k4txqed4cj2hx3i4kck3xbnbgxfm4asy3kzsrhzqa3ua",
+    attributes: [
+      { trait_type: "Type", value: "Bug/Steel" },
+      { trait_type: "Power", value: 88 },
+    ],
+  },
+  {
+    name: "Cyber-Gengar Phantom",
+    description: "A digital glitch given form, haunting the cloud networks and consuming data packets.",
+    image:
+      "https://azure-voluntary-cat-524.mypinata.cloud/ipfs/bafybeigyz46rmacnj2ao3wknviwo6nkci4cl4e56vhvzfmk3ksrxs7w37u",
+    attributes: [
+      { trait_type: "Type", value: "Ghost/Cyber" },
+      { trait_type: "Power", value: 94 },
+    ],
+  },
+  {
+    name: "Apex Mecha-Raptor",
+    description: "A relentless tracking unit with thermal vision and heat-seeking sonic capabilities.",
+    image:
+      "https://silver-hollow-goldfish-48.mypinata.cloud/ipfs/bafybeid4tf5kmwwdj5umvddkxy6y5izhtfhrsonmbafrwe2c52w5yq25fi",
+    attributes: [
+      { trait_type: "Type", value: "Steel/Normal" },
+      { trait_type: "Power", value: 82 },
+    ],
+  },
+  {
+    name: "Blue-Streak Pidgeot",
+    description: "A high-altitude reconnaissance drone that leaves a trail of frozen vapor in its wake.",
+    image:
+      "https://silver-hollow-goldfish-48.mypinata.cloud/ipfs/bafybeigcwycxphwquzly43j26papu2i7kkg4kknih4sx54jpglldljm6o4",
+    attributes: [
+      { trait_type: "Type", value: "Flying/Ice" },
+      { trait_type: "Power", value: 76 },
+    ],
+  },
+  {
+    name: "Crystal-Scale Tyranitar",
+    description: "A prehistoric juggernaut reinforced with violet crystal plating and core-fission energy.",
+    image:
+      "https://silver-hollow-goldfish-48.mypinata.cloud/ipfs/bafybeicrculjz6vu4c37d3nlygkxd3dhfkahuzdag5xen2mdnulsm7hcmq",
+    attributes: [
+      { trait_type: "Type", value: "Rock/Dark" },
+      { trait_type: "Power", value: 115 },
+    ],
+  },
+  {
+    name: "Combat-Ready Lucario",
+    description: "A specialized martial-arts bot that uses internal data processors to predict enemy moves.",
+    image:
+      "https://silver-hollow-goldfish-48.mypinata.cloud/ipfs/bafybeifucyru65mt2ocfaycoecvqdsncdhhyxcl37lpie3j44xviqzkrra",
+    attributes: [
+      { trait_type: "Type", value: "Fighting/Steel" },
+      { trait_type: "Power", value: 98 },
+    ],
+  },
+  {
+    name: "Nano-Bloom Bulbasaur",
+    description: "A biotech symbiotic unit that uses solar panels on its back to power its leaf-cannons.",
+    image:
+      "https://silver-hollow-goldfish-48.mypinata.cloud/ipfs/bafybeibk6vzfucbj7i3ykvthnrv66pd3bmccx6emoxfye576jleigy3edi",
+    attributes: [
+      { trait_type: "Type", value: "Grass/Tech" },
+      { trait_type: "Power", value: 65 },
+    ],
+  },
+  {
+    name: "Liquid-Neon Dratini",
+    description: "A rare aquatic prototype made of semi-transparent smart-fluids and glowing sensors.",
+    image:
+      "https://silver-hollow-goldfish-48.mypinata.cloud/ipfs/bafybeiewqkobt4b6cfclpgp3cor6m633xivcdb4o72xio3ckrhlbh5azpm",
+    attributes: [
+      { trait_type: "Type", value: "Water/Dragon" },
+      { trait_type: "Power", value: 50 },
+    ],
+  },
+  {
+    name: "Hydro-Glass Vaporeon",
+    description: "A high-pressure aquatic unit that can dissolve into water for stealth infiltration.",
+    image:
+      "https://bronze-perfect-loon-108.mypinata.cloud/ipfs/bafybeiaahr4oguq2zumjwq2jtb3ydhro6brrw7envkx2smabd4tv376f4e",
+    attributes: [
+      { trait_type: "Type", value: "Water/Glass" },
+      { trait_type: "Power", value: 80 },
+    ],
+  },
+  {
+    name: "Heavy-Artillery Blastoise",
+    description: "A walking fortress armed with dual dual-railgun hydro-cannons and blast plating.",
+    image:
+      "https://bronze-perfect-loon-108.mypinata.cloud/ipfs/bafybeifolj7xn7pzscpz42vxk6lhs3mjasaoghk4ejnf753tqbpe5l4cte",
+    attributes: [
+      { trait_type: "Type", value: "Water/Steel" },
+      { trait_type: "Power", value: 130 },
+    ],
+  },
+  {
+    name: "Obsidian-Steel Onix",
+    description: "A massive, drill-tipped serpent forged from reinforced obsidian and hydraulic gears.",
+    image:
+      "https://bronze-perfect-loon-108.mypinata.cloud/ipfs/bafybeicayg46xies52kukpesw2gsc7aigeio6cicm26pqy6rf3jr5pkmba",
+    attributes: [
+      { trait_type: "Type", value: "Rock/Steel" },
+      { trait_type: "Power", value: 95 },
+    ],
+  },
+  {
+    name: "Jolteon Overdrive",
+    description: "An agility-focused unit capable of moving at the speed of light by overclocking its core.",
+    image:
+      "https://bronze-perfect-loon-108.mypinata.cloud/ipfs/bafybeigk3ylupmpenddauhqdhlq5owxanmvnbgxavlvv3uhyhp2yqvkd24",
+    attributes: [
+      { trait_type: "Type", value: "Electric/Tech" },
+      { trait_type: "Power", value: 89 },
+    ],
+  },
+  {
+    name: "Titan-Shell Lapras",
+    description: "An armored aquatic transport fitted with deep-sea sonar and sub-zero defensive turrets.",
+    image:
+      "https://bronze-perfect-loon-108.mypinata.cloud/ipfs/bafybeih65mbuivekh7osqmgbzyvfrvw73jawhscxxgxvyvn7wmoquosybm",
+    attributes: [
+      { trait_type: "Type", value: "Water/Ice" },
+      { trait_type: "Power", value: 110 },
+    ],
+  },
+  // {
+  //   name: "Void-Stalker Mewtwo",
+  //   description: "The ultimate genetic experiment, fitted with a dark-matter engine and reality-bending sensors.",
+  //   image: "https://bronze-perfect-loon-108.mypinata.cloud/ipfs/bafybeiaixr4gpgb723oex66ktw7unquksvkj6svrljgu7v4pdb6hod5akm",
+  //   attributes: [{ trait_type: "Type", value: "Psychic/Void" }, { trait_type: "Power", value: 180 }]
+  // },
+  {
+    name: "Nano-Sprite Mew",
+    description: "A playful, semi-transparent entity containing the digital blueprints of every known Pokémon.",
+    image:
+      "https://black-impossible-cockroach-770.mypinata.cloud/ipfs/bafybeibhnqyynkviugriubow5e5wykpr7nrtl6x3lwa6pirrbxtskewecm",
+    attributes: [
+      { trait_type: "Type", value: "Psychic/Origin" },
+      { trait_type: "Power", value: 150 },
+    ],
+  },
+  {
+    name: "Bloom-Fortress Venusaur",
+    description: "A heavy-siege botanic unit that converts carbon-emissions into concentrated solar beams.",
+    image:
+      "https://black-impossible-cockroach-770.mypinata.cloud/ipfs/bafybeiaern5pz37g2uh4t64f3rmxz6ubyfjuvj7tgn2s3ssbaao24ql6sa",
+    attributes: [
+      { trait_type: "Type", value: "Grass/Poison" },
+      { trait_type: "Power", value: 125 },
+    ],
+  },
+  {
+    name: "Ether-Drake Gyarados",
+    description: "A legendary sovereign of the skies and seas, born from pure code and raw destructive energy.",
+    image:
+      "https://black-impossible-cockroach-770.mypinata.cloud/ipfs/bafybeicfvihly6u3lknfxpwnqt7uey6wg5s62q62mq7a2ublblla5if7mu",
+    attributes: [
+      { trait_type: "Type", value: "Water/Dragon" },
+      { trait_type: "Power", value: 195 },
+    ],
+  },
+  // {
+  //   name: "Mecha-Charizard Prime",
+  //   description: "The apex fire-type unit, using a dual-turbine flight system to rain down blue plasma fire.",
+  //   image: "https://black-impossible-cockroach-770.mypinata.cloud/ipfs/bafybeid4tqyemyl4vtx5ugpocarpyjui5jgogc2dd72gu25egogurbc3oi",
+  //   attributes: [{ trait_type: "Type", value: "Fire/Flying" }, { trait_type: "Power", value: 175 }]
+  // },
+  {
+    name: "Cyber-Pikachu Proto",
+    description: "The original electric prototype, upgraded with bioluminescent sensors and high-frequency whiskers.",
+    image:
+      "https://black-impossible-cockroach-770.mypinata.cloud/ipfs/bafybeiaudghko7ggfnfqmn5f6deaynl6cnsbkj5bfxnl5m7vmadypehkmq",
+    attributes: [
+      { trait_type: "Type", value: "Electric" },
+      { trait_type: "Power", value: 105 },
+    ],
+  },
+  {
+    name: "Ribbon-Link Sylveon-X",
+    description: "A sleek cyber-feline that uses digital ribbons to hack into enemy systems and pacify them.",
+    image:
+      "https://black-impossible-cockroach-770.mypinata.cloud/ipfs/bafybeihnjh3d7agbqg6plko36qrjai2alqgfpug4jwn2axxq4yij2gnx24",
+    attributes: [
+      { trait_type: "Type", value: "Fairy/Cyber" },
+      { trait_type: "Power", value: 88 },
     ],
   },
 ];
