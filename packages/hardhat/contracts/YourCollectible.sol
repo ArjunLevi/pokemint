a944bfb238600af407dd09becae4c746e0df050b
ArjunLevi <66naga18@gmail.com>
2026-02-27 11:50:54 +0530

    Up[date the Contract TO my PokeMint

--- packages/hardhat/contracts/YourCollectible.sol
+++ packages/hardhat/contracts/YourCollectible.sol
@@ -8,28 +8,48 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract YourCollectible is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     uint256 public tokenIdCounter;
+    
+    // 0.01 USD is roughly 0.000005 ETH (adjust this as needed)
+    uint256 public mintFee = 0.000005 ether;
 
-    constructor() ERC721("YourCollectible", "YCB") Ownable(msg.sender) {}
+    constructor() ERC721("YourCollectible", "PKN") Ownable(msg.sender) {}
 
     function _baseURI() internal pure override returns (string memory) {
         return "https://ipfs.io/ipfs/";
     }
 
-    function mintItem(address to, string memory uri) public returns (uint256) {
+    // Notice the 'payable' modifier - this allows the function to receive money
+    function mintItem(address to, string memory uri) public payable returns (uint256) {
+        require(msg.value >= mintFee, "Not enough ETH sent; check the mint fee");
+
         tokenIdCounter++;
         uint256 tokenId = tokenIdCounter;
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
+        
         return tokenId;
     }
 
-    // Override functions from OpenZeppelin ERC721, ERC721Enumerable and ERC721URIStorage
+    // --- FEE MANAGEMENT FUNCTIONS ---
 
-    function _update(
-        address to,
-        uint256 tokenId,
-        address auth
-    ) internal override(ERC721, ERC721Enumerable) returns (address) {
+    // Allow the owner to change the fee if the price of ETH changes
+    function setMintFee(uint256 _newFee) public onlyOwner {
+        mintFee = _newFee;
+    }
+
+    // This is how you get the money OUT of the contract
+    function withdraw() public onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "No funds to withdraw");
+        
+        // Sends all accumulated fees to the owner's wallet
+        (bool success, ) = payable(owner()).call{value: balance}("");
+        require(success, "Transfer failed");
+    }
+
+    // --- OVERRIDES REQUIRED BY SOLIDITY ---
+
+    function _update(address to, uint256 tokenId, address auth) internal override(ERC721, ERC721Enumerable) returns (address) {
         return super._update(to, tokenId, auth);
     }
 
@@ -41,9 +61,7 @@ contract YourCollectible is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable 
         return super.tokenURI(tokenId);
     }
 
-    function supportsInterface(
-        bytes4 interfaceId
-    ) public view override(ERC721, ERC721Enumerable, ERC721URIStorage) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable, ERC721URIStorage) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 }
